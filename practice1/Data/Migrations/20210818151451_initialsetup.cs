94e37a7bacd29a5b72a3e6411357eaf396eb626f
Hiruni Perera <48178096+HiruniPerera@users.noreply.github.com>
2021-08-22 16:59:12 +0530

    Add project files.

---
+++ practice1/Data/Migrations/20210818151451_initialsetup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace practice1.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "joke",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    JokeQuestion = table.Column<string>(nullable: true),
+                    JokeAnswer = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_joke", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "joke");
+        }
+    }
+}
---
+++ practice1/Models/joke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace practice1.Models
+{
+	public class joke
+	{
+		public int Id { get; set; }
+		public String JokeQuestion { get; set; }
+		public String JokeAnswer { get; set; }
+		public joke()
+		{
+
+		}
+	}
+
+}
